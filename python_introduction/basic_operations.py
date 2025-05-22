015aa0e7e4c66b61516c04852e29af153b59db5c
Tobi Kareem <Tobiazkareem@gmail.com>
2025-05-22 13:39:22 +0100

    operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#Define the variables for basic operations
+number1 = 10
+number2 = 5
+
+#Assihn operations
+subtract = number1 - number2
+multiply = number1 * number2
+add = number1 + number2
+
+print("Addition of", number1, "and", number2, "is", add)
+print("Substraction of", number1, "and", number2, "is", subtract)
+print("Multiplication of", number1, "and", number2, "is", multiply)
