99cd1f185468c62f2ec12d6d7678e57f47bd95c5
Tobi Kareem <Tobiazkareem@gmail.com>
2025-05-22 13:55:11 +0100

    task operations

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+#rectangle area.py
+
+#Define the l and w variables
+
+length = 10
+width = 5
+
+#Calculate the area
+area = length * width
+
+#print the calculated area
+print("The area of the rectangle is:", area)
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+simple interest
+
+#Define the three variables
+principal = 1000 #representing $1000
+rate = 0.05 #representing 5% annual interest rate
+time = 3 #representing 3 years
+
+#calculate the simple interest
+interest = principal * rate * time
+
+#print interest
+print("The simple interest is:", interest)
